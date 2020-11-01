84931693eecec0f3c89638ab5070f393422659f4
Daniel Adea <daniel.adea@risksciences.ucla.edu>
2020-10-31 22:53:29 -0700

    init repo

--- CRC.cpp
+++ CRC.cpp
@@ -4,14 +4,45 @@
 const uint64_t CRC::m_polynomial = 0x42F0E1EBA9EA3693ull;
 
 CRC::CRC()
-{}
+{
+	create_crc_table();
+}
 
 CRC::~CRC()
 {}
 
 void CRC::create_crc_table()
-{}
+{
+	uint64_t generator = m_polynomial;
+	crctable = new uint64_t[256];
+	/* iterate over all byte values 0 - 255 */
+	for (int dividend = 0; dividend < 256; dividend++)
+	{
+		uint64_t crc = (uint64_t) dividend;
+		/* calculate the CRC-8 value for current byte */
+		for (uint64_t bit = 0; bit < 8; bit++)
+		{
+			if ((crc & 0x8000000000000) != 0)
+			{
+				crc <<= 1;
+				crc ^= generator;
+			}
+			else
+			{
+				crc <<= 1;
+			}
+		}
+		/* store CRC value in lookup table */
+		crctable[dividend] = crc;
+	}
+}
 
 uint64_t CRC::get_crc_code(uint8_t *stream, int length)
-{}
+{
+	uint64_t rem = 0;
+	for (i = 0; i<length; i++)
+	{
+		rem = (rem << 8) ^ m_CRC_table[stream[i] ^ (rem >> 56)]
+	}
+}
 
--- client.cpp
+++ client.cpp
@@ -1,6 +1,18 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+
+#include <iostream>
+#include <sstream>
 
 #include "CRC.h"
 
@@ -9,8 +21,49 @@ using namespace std;
 #define BUFSIZE 1024
 #define CRCSIZE 8
 
+void checkErr(int testVal, char* message)
+{
+//usage if testVal<0, print message with strerr and exit
+  if(testVal<0)
+  {
+    fprintf(stderr, "Error: %s . %s \n",strerror(errno), message);
+  }
+}
+
+
 int main(int argc, char* argv[]) 
 {
-    cerr << "client is not implemented yet" << endl;
+
+	
+	string hostname = argv[1]; 
+	int portVal = argv[2];
+	string filename = argv[3];
+//
+	struct sockaddr_in serv_addr;
+  	struct hostent *server; 
+  	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  	checkErr(sockfd, "Failed to create socket");
+
+ 	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(portVal);
+
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+	int connectTest = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+	checkErr(connectTest, "Failed to connect socket");
+//
+	//char buffer[1024];
+	//ifstream myFile("filename");
+
+
+
+
+
+	
+
+	close(sockfd);
+
+
     return 0;
 }
